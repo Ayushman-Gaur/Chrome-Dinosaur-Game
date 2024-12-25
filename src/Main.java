b812628195f1fc8efb90c4adc608d956bfaad980
Ayushman-Gaur <120575155+Ayushman-Gaur@users.noreply.github.com>
2024-12-25 19:58:10 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int boardHeight= 250;
+        int boardWidth=750;
+
+        JFrame frame = new JFrame("Chrome Dinosaur");
+        frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
